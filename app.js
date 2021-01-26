debec2304dcadd4e0b8655466c5cf1e078fffa07
MichaelaVop <70598230+MichaelaVop@users.noreply.github.com>
2021-01-26 21:39:26 +0100

    styles added

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ form.addEventListener('submit', function(e){
     
     const tweetTxt = document.querySelector('ul');
     const makeTwtList = document.createElement('li');
+    
     makeTwtList.innerHTML = '<span>' + form.username.value + '</span>' + ": " + form.tweet.value;
-    makeTwtList.style.fontStyle = 'italic';
+    
+    makeTwtList.style.listStyle = 'none';
     tweetTxt.appendChild(makeTwtList);
+    const userName = document.querySelector('span');
+    userName.style.fontStyle = 'italic';
 
 });
